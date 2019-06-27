a53c40da56e1735645031fd489de680767c98ef1
Don Blair <donblair@gmail.com>
2019-06-26 20:14:13 -0400

    update

--- quahog/rad_relay.py
+++ quahog/rad_relay.py
@@ -19,8 +19,12 @@ TIMEOUT = 1.
 # sensor data + url
 url_base='http://mosspig.club/?public_key=834c74e03901cd1702c0a3060803f767&private_key=bfe468dc77b5530d65319b67cc39cdbc'
 
+FONA_RX=19
+FONA_TX=21
+BAUDRATE=9600
+CELL_PRE_SLEEP=5
 
-
+uart = UART(1, baudrate=BAUDRATE,rx=FONA_RX,tx=FONA_TX,timeout=10)
 
 
 i2c = I2C(-1, Pin(14), Pin(2))
@@ -46,13 +50,81 @@ rfm9x = RFM9x(spi, cs, resetNum, 915.0)
 baudrate=9600
 #baudrate=57600
 
+def relay_post(url):
+	time.sleep(CELL_PRE_SLEEP)
+	uart.write("AT+SAPBR=0,1\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+SAPBR=3,1,\"APN\",\"fast.t-mobile.com\"\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+CREG?\r")
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+SAPBR=2,1\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+SAPBR=1,1\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("WAIT=6\r")
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+CMGF=0\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+HTTPINIT\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+
+	uart.write("AT+HTTPPARA=\"URL\",\""+url+"\"\r")
+
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+HTTPPARA=\"CID\",1\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+
+	uart.write("AT+HTTPACTION=0\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+HTTPREAD\r")
+	time.sleep(1)
+	a=uart.readline()
+	b=uart.readline()
+	print(a)
+	print(b)
+
+	oled.fill(0)
+	oled.text(a,0,0)
+	oled.show()
+
+	uart.write("WAIT=6\r")
+	print(uart.readline())
+	print(uart.readline())
+	uart.write("AT+HTTPTERM\r")
+	time.sleep(1)
+	print(uart.readline())
+	print(uart.readline())
+
 while True:
 
+	print("radio waiting ...")
 	OLED_CURRENTLINE=0
 	oled.fill(0)
 	OLED_CURRENTLINE=OLED_CURRENTLINE+OLED_LINESKIP
 	oled.text("Radio waiting ...",0,OLED_CURRENTLINE)
 	oled.show()
+	
 
 	rfm9x.receive(timeout=TIMEOUT)
 	if rfm9x.packet is not None:
@@ -71,14 +143,21 @@ while True:
 			print('Received: {}'.format(packet_text))
 			print("RSSI: {}".format(rssi))
 			OLED_CURRENTLINE=OLED_CURRENTLINE+OLED_LINESKIP
-			oled.text("(would post here)",0,OLED_CURRENTLINE)
+			oled.text("Posting ...",0,OLED_CURRENTLINE)
 			oled.show() 
-			time.sleep(2)
-			#url_full=url_base+'&temp='+str(temp)+'&moisture='+str(moisture)
-			#print(url_full)
-			#relay_post_code()
-		except:
-			print("some error?")
+			params=packet_text.split(",")
+			if len(params)==2:
+				print(params)
+				temp=params[0]
+				moisture=params[1]
+				url_full=url_base+'&temp='+str(temp)+'&moisture='+str(moisture)
+				print(url_full)
+				relay_post(url_full)
+			else:
+				print("weird ...")
+		except Exception as e:
+			print("error: "+str(e))
+			#print("some error?")
 			#display_text = "[{}]: (garbled msg)".format(i)
 			#oled.text(display_text,0,0)
 	gc.collect()
